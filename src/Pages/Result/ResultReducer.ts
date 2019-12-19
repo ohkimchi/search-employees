05604c52d4fea0003d95751acff56d6c52aef586
max <maxjunhuixu@gmail.com>
2019-12-20 00:28:14 +0800

    improve

--- src/Pages/Result/ResultReducer.ts
+++ src/Pages/Result/ResultReducer.ts
@@ -8,7 +8,7 @@ export const useStyles = makeStyles((theme: Theme) =>
     },
     demo: {
       backgroundColor: theme.palette.background.paper,
-      width: "50%",
+      width: "85%",
       margin: "auto"
     }
   })
--- src/Pages/Search/Search.tsx
+++ src/Pages/Search/Search.tsx
@@ -2,73 +2,79 @@ import IconButton from "@material-ui/core/IconButton"
 import InputBase from "@material-ui/core/InputBase"
 import Paper from "@material-ui/core/Paper"
 import SearchIcon from "@material-ui/icons/Search"
-import React, { FC, useContext, useEffect, useState } from "react"
-import { Context, ActionType } from "../../App/AppReducer"
-import { API } from "../../utils/api"
+import { debounce } from "lodash"
+import React, { FC, useContext, useState } from "react"
+import { ActionType, Context } from "../../App/AppReducer"
+import { getDirectSub } from "../../utils/directSub"
 import { useStyles } from "./SearchReducer"
 import "./_search.scss"
-import { debounce } from "lodash"
-import { getDirectSub } from "../../utils/directSub"
 
 const data = [
   "CEO",
   {
-    "direct-subordinates": [
-      "Samad Pitt",
-      "Leanna Hogg"
-    ]
+    "direct-subordinates": ["Samad Pitt", "Leanna Hogg"]
   }
 ]
 const Search: FC = () => {
   const classes = useStyles()
   const { state, dispatch } = useContext(Context)
   const [name, setName] = useState("")
+  const [showReminder, setShowReminder] = useState(false)
 
   const handleOnChange = debounce(async (name: string) => {
-    setName(name)
-    const dirSubRes = await getDirectSub(name)
-    // console.log(dirSubRes)
-    if (dirSubRes) {
-      let [role, dirSubObj] = dirSubRes as any
-      const subArr = dirSubObj["direct-subordinates"]
-      dispatch({
-        directSub: subArr,
-        type: ActionType.SET_DIRECT_SUB
-      })
-      dispatch({
-        employeeName: name,
-        type: ActionType.SET_EMPLOYEE_NAME
-      })
+    if (name !== "") {
+      setName(name)
+      const dirSubRes = await getDirectSub(name)
+      // console.log(dirSubRes)
+      if (dirSubRes) {
+        let [role, dirSubObj] = dirSubRes as any
+        const subArr = dirSubObj["direct-subordinates"]
+        dispatch({
+          directSub: subArr,
+          type: ActionType.SET_DIRECT_SUB
+        })
+        dispatch({
+          employeeName: name,
+          type: ActionType.SET_EMPLOYEE_NAME
+        })
+      }
     }
   }, 300)
 
-  function handleOnClick () {
-    dispatch({
-      currentPage: "ResultPage",
-      type: ActionType.SET_CURRENT_PAGE
-    })
+  function handleOnClick(e: any) {
+    e.preventDefault()
+    if (name === "") {
+      setShowReminder(true)
+    } else {
+      // setShowReminder(false)
+      console.log("name", name)
+      dispatch({
+        currentPage: "ResultPage",
+        type: ActionType.SET_CURRENT_PAGE
+      })
+    }
   }
-
 
   return (
     <div className="search-page">
       <p>Employee Explorer</p>
-      <Paper component="form" className={ classes.root }>
+      <Paper component="form" className={classes.root}>
         <InputBase
-          className={ classes.input }
+          className={classes.input}
           placeholder="Search Employee Name"
-          inputProps={ { "aria-label": "search employee name" }}
-          onChange={(e) => handleOnChange(e.target.value)}
+          inputProps={{ "aria-label": "search employee name" }}
+          onChange={e => handleOnChange(e.target.value)}
         />
         <IconButton
           type="submit"
-          className={ classes.iconButton }
+          className={classes.iconButton}
           aria-label="search"
-          onClick={() => handleOnClick()}
+          onClick={e => handleOnClick(e)}
         >
           <SearchIcon />
         </IconButton>
       </Paper>
+      {showReminder && <p>You should key in something then search.</p>}
     </div>
   )
 }
